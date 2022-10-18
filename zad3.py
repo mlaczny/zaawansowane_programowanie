f2b5b301613b57a5d90118dbc10639552aeb0813
mlaczny <56277278+mlaczny@users.noreply.github.com>
2022-10-18 22:13:18 +0200

    Delete zad3.py

--- zad3.py
+++
@@ -1,8 +0,0 @@
-def even_number(number) -> bool:
-    return number % 2 == 0
-
-
-x = even_number(5)
-print("Liczba parzysta") if x else print("Liczba nieparzysta")
-y = even_number(4)
-print("Liczba parzysta") if y else print("Liczba nieparzysta")
