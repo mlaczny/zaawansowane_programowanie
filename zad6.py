c61caf5dc015fb228866c9730351bb59fc1f9e16
mlaczny <56277278+mlaczny@users.noreply.github.com>
2022-10-18 22:12:44 +0200

    Delete zad6.py

--- zad6.py
+++
@@ -1,8 +0,0 @@
-def add_list(first: list, second: list) -> list:
-    third = first + second
-    return [x ** 3 for x in list(set(third))]
-
-
-first = [3, 464, 67, 3, 8, 2, 758, 26, 0]
-second = [3, 464, 6, 245, 4563, 7, 643, 3, 8, 2, 758, 26, 0]
-print(add_list(first, second))
